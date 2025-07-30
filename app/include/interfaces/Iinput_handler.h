1f19d6158dd01fb9324479177ef106ad3eefbae5
Nazar Vakulenko <nazardrakulito@gmail.com>
2025-07-30 12:09:46 +0300

    feat: add interfaces and CMakeLists.txt

---
+++ app/include/interfaces/Iinput_handler.h
@@ -0,0 +1,8 @@
+//
+// Created by nazar on 30.07.2025.
+//
+
+#ifndef IINPUT_HANDLER_H
+#define IINPUT_HANDLER_H
+
+#endif //IINPUT_HANDLER_H
---
+++ app/include/interfaces/Ilog_worker.h
@@ -0,0 +1,8 @@
+//
+// Created by nazar on 30.07.2025.
+//
+
+#ifndef ILOG_WORKER_H
+#define ILOG_WORKER_H
+
+#endif //ILOG_WORKER_H
