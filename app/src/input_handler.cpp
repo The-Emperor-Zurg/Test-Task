3d3cdd7f4630c59f9cb94e4d3d9da552b427b7dd
Nazar Vakulenko <nazardrakulito@gmail.com>
2025-07-30 13:47:19 +0300

    feat: add input_handler.cpp

--- app/src/input_handler.cpp
+++ app/src/input_handler.cpp
@@ -1,3 +1,91 @@
-//
-// Created by nazar on 30.07.2025.
-//
+#include <sstream>
+
+#include "input_handler.h"
+
+#include <algorithm>
+#include <vector>
+
+
+namespace LoggerApp {
+    std::optional<Message> InputHandler::parseInput(const std::string &input, MyLogger::LogLevel defaultLevel) const {
+        std::string trimmedInput = trim(input);
+
+        if (trimmedInput.empty()) {
+            return std::nullopt;
+        }
+
+        if (trimmedInput == "exit" || trimmedInput == "quit" || trimmedInput == "q") {
+            return Message::createExitMessage();
+        }
+
+        std::istringstream stream(trimmedInput);
+        std::string word;
+        std::vector<std::string> words;
+
+        while (stream >> word) {
+            words.emplace_back(word);
+        }
+
+        if (words.empty()) {
+            return std::nullopt;
+        }
+
+        std::string messageText;
+        MyLogger::LogLevel logLevel = defaultLevel;
+
+        auto lastWordLevel = parseLogLevel(words.back());
+        if (lastWordLevel.has_value()) {
+            logLevel = lastWordLevel.value();
+            for (size_t i = 0; i < words.size() - 1; i++) {
+                if (i > 0) {
+                    messageText += " ";
+                }
+
+                messageText += words[i];
+            }
+        } else {
+            for (size_t i = 0; i < words.size(); ++i) {
+                if (i > 0) {
+                    messageText += " ";
+                }
+
+                messageText += words[i];
+            }
+        }
+
+        if (messageText.empty()) {
+            return std::nullopt;
+        }
+
+        return Message(messageText, logLevel);
+    }
+
+    std::optional<MyLogger::LogLevel> InputHandler::parseLogLevel(const std::string &level) const {
+        std::string upperLevel = level;
+        std::transform(upperLevel.begin(), upperLevel.end(), upperLevel.begin(), ::toupper);
+
+        if (upperLevel == "INFO") {
+            return MyLogger::LogLevel::INFO;
+        }
+
+        if (upperLevel == "SECRET_INFO") {
+            return MyLogger::LogLevel::SECRET_INFO;
+        }
+
+        if (upperLevel == "TOP_SECRET_INFO") {
+            return MyLogger::LogLevel::TOP_SECRET_INFO;
+        }
+
+        return std::nullopt;
+    }
+
+    std::string InputHandler::trim(const std::string& input) const {
+        auto start = input.find_first_not_of(" \n\r\t");
+        if (start == std::string::npos) {
+            return "";
+        }
+
+        auto end = input.find_last_not_of(" \n\r\t");
+        return input.substr(start, end - start + 1);
+    }
+}
